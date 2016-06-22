82aa2299db9a4ee5c40a15431a7f1ba10d17bbb2
Sumit Aggarwal <aggarwal.s@samsung.com>
2016-06-22 16:38:10 +0530

    [connection]: Added new CAPI's Prototype.
    a./ connection_profile_get_dns_config_type()
    b./ connection_profile_get_prefix_length()
    c./ connection_profile_set_dns_config_type()
    d./ connection_profile_set_prefix_length()
    Signed-off-by: Sumit Aggarwal <aggarwal.s@samsung.com>
    
    Change-Id: I89311793bf54e6707c5069cfa1ede35773245574

--- include/connection_profile.h
+++ include/connection_profile.h
@@ -180,6 +180,17 @@ typedef enum{
 } connection_profile_type_e;
 
 /**
+ * @brief Enumeration for DNS configuration type.
+ * @since_tizen 3.0
+*/
+typedef enum
+{
+    CONNECTION_DNS_CONFIG_TYPE_NONE = 0,  /**< Not defined */
+    CONNECTION_DNS_CONFIG_TYPE_STATIC  = 1,  /**< Manual DNS configuration */
+    CONNECTION_DNS_CONFIG_TYPE_DYNAMIC = 2,  /**< Config DNS using DHCP client*/
+} connection_dns_config_type_e;
+
+/**
  * @brief Creates a profile handle.
  * @details The profile name, which you get from connection_profile_get_name(), will include the keyword you set.
  * @since_tizen @if MOBILE 2.3 @elseif WEARABLE 2.3.1 @endif
@@ -928,6 +939,66 @@ int connection_profile_set_cellular_pdn_type(connection_profile_h profile, conne
  * @retval #CONNECTION_ERROR_NOT_SUPPORTED     Not Supported
 */
 int connection_profile_set_cellular_roam_pdn_type(connection_profile_h profile, connection_cellular_pdn_type_e type);
+
+/**
+* @brief Gets the DNS config type.
+* @since_tizen @if TV 3.0 @endif
+* @param[in] profile  The handle of profile
+* @param[in] address_family  The address family
+* @param[out] type  The type of DNS config
+* @return @c 0 on success, otherwise negative error value.
+* @retval #CONNECTION_ERROR_NONE  Successful
+* @retval #CONNECTION_ERROR_INVALID_PARAMETER  Invalid parameter
+* @retval #CONNECTION_ERROR_OPERATION_FAILED  Operation failed
+* @retval #CONNECTION_ERROR_NOT_SUPPORTED     Not Supported
+*/
+int connection_profile_get_dns_config_type(connection_profile_h profile,
+		connection_address_family_e address_family,
+		connection_dns_config_type_e* type);
+
+/**
+* @brief Sets the DNS config type.
+* @since_tizen @if TV 3.0 @endif
+* @param[in] profile  The handle of profile
+* @param[in] address_family  The address family
+* @param[in] type  The type of DNS config
+* @return @c 0 on success, otherwise negative error value.
+* @retval #CONNECTION_ERROR_NONE  Successful
+* @retval #CONNECTION_ERROR_INVALID_PARAMETER  Invalid parameter
+* @retval #CONNECTION_ERROR_OPERATION_FAILED  Operation failed
+* @retval #CONNECTION_ERROR_NOT_SUPPORTED     Not Supported
+* @see connection_update_profile()
+*/
+int connection_profile_set_dns_config_type(connection_profile_h profile,
+		connection_address_family_e address_family,
+		connection_dns_config_type_e type);
+
+/**
+* @brief Gets the Prefix Length of IPv6.
+* @since_tizen @if TV 3.0 @endif
+* @param[in] profile  The handle of profile
+* @param[out] prefix_len  The prefix length
+* @return @c 0 on success, otherwise negative error value.
+* @retval #CONNECTION_ERROR_NONE  Successful
+* @retval #CONNECTION_ERROR_INVALID_PARAMETER  Invalid parameter
+* @retval #CONNECTION_ERROR_OPERATION_FAILED  Operation failed
+* @retval #CONNECTION_ERROR_NOT_SUPPORTED     Not Supported
+*/
+int connection_profile_get_prefix_length(connection_profile_h profile, unsigned char* prefix_length);
+
+/**
+* @brief Sets the Prefix Length of IPv6.
+* @since_tizen @if TV 3.0 @endif
+* @param[in] profile  The handle of profile
+* @param[in] prefix_length  The prefix len. If you set this value to NULL, then the existing value will be deleted.
+* @return @c 0 on success, otherwise negative error value.
+* @retval #CONNECTION_ERROR_NONE  Successful
+* @retval #CONNECTION_ERROR_INVALID_PARAMETER  Invalid parameter
+* @retval #CONNECTION_ERROR_OPERATION_FAILED  Operation failed
+* @retval #CONNECTION_ERROR_NOT_SUPPORTED     Not Supported
+* @see connection_update_profile()
+*/
+int connection_profile_set_prefix_length(connection_profile_h profile, unsigned char prefix_length);
 
 /**
 * @}
